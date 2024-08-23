19b87fa743572198df4b24db383bc754c825ae97
Anne Caroline <carolaragao94@gmail.com>
2024-08-23 10:31:03 -0300

    Pages Home e SignUp

---
+++ src/pages/Home.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function HomeScreen ({ navigation }) {
+    return (
+        <View style={styles.container}>
+          <Text style={styles.text}>Vamos criar algo novo juntos.</Text>
+          
+          <TouchableOpacity style={styles.button} activeOpacity={0.7} onPress={() => navigation.navigate('SignUp')}>
+            <Text style={styles.buttonText}>Crie uma conta</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+            <Text style={styles.buttonText}>Entre</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+    
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f0f0f0'
+      },
+      text: {
+        fontSize: 18,
+        marginBottom: 20,
+        color: '#333'
+      },
+      button: {
+        backgroundColor: '#6200EE',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+        marginTop: 10,
+        width: '80%',
+        alignItems: 'center',
+      },
+      buttonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+      },
+    });
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "react-navigation/stack";
+import HomeScreen from "../pages/Home";
+import SignUpScreen from "../pages/SignUp";
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
+                <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: 'Criar Conta'}} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
