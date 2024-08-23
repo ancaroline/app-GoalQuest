760723654937e30118e102659b963d7b3d163714
Anne Caroline <carolaragao94@gmail.com>
2024-08-23 09:47:50 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,43 @@
+import { Text, View, StyleSheet, TouchableOpacity, Button } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Vamos criar algo novo juntos.</Text>
+      
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>Entre</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>Crie uma conta</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0'
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 20,
+    color: '#333'
+  },
+  button: {
+    backgroundColor: '#6200EE',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 10,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+  },
+});
